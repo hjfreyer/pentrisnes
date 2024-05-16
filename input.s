805759dce42983ecdd870ecdd6e5cdd1a54485f4
Hunter Freyer <yt@hjfreyer.com>
2024-05-16 09:12:41 -0400

    Rotation support and cleanup

--- input.s
+++ input.s
@@ -17,16 +17,12 @@ INPUT_STATUS_D_PRESSED  = $1000
 D_PAD_PRESSED   = $1000
 D_PAD_HELD      = $2000
 
-; PRE_HELD        
-
 DAS_INITIAL_DELAY  = $0010
 DAS_REPEAT      = $0006
 
 .BSS
 PrevInput:      .res 2
-; ButtonHeld      .res 2
 DasTimer:         .res 2
-; DasRepeat       .res 2
 
 .CODE
 
@@ -50,6 +46,20 @@ DasTimer:         .res 2
 
     jsr ReadInput
 
+    ; Handle rotation.
+    lda $03, S      ; Load input status
+    bit #INPUT_STATUS_ROTATE
+    bne Rotate
+    jmp RotateEnd
+
+    ; TODO: Counter rotation.
+
+Rotate:
+    jsr TryRotate
+
+RotateEnd:
+
+    ; Handle translation.
     lda $03, S      ; Load input status
     bit #INPUT_STATUS_D_PRESSED
     bne DoPressed
@@ -57,89 +67,34 @@ DasTimer:         .res 2
     bit #INPUT_STATUS_D_HELD
     bne DoHeld
 
-    jmp End
-
-DoHeld:
-    lda DasTimer
-    beq DoDas
-
-    dec
-    sta DasTimer
-
-    jmp End
-
-DoDas:
-    jsr TryMove
-
-    lda #DAS_REPEAT
-    sta DasTimer
-
-    jmp End
+    jmp TranslateEnd
 
 DoPressed:
-    jsr TryMove
+    jsr TryMove             ; Try to move by the delta already on the stack.
 
-    lda #DAS_INITIAL_DELAY
+    lda #DAS_INITIAL_DELAY  ; Reset DAS timer to initial delay.
+    sta DasTimer
+    jmp TranslateEnd
+
+DoHeld:
+    lda DasTimer        ; If the button is held and the DAS timer expired...
+    beq DoDas           ; do the DAS thing.
+
+    dec                 ; Otherwise decrement the timer and continue.
+    sta DasTimer
+    jmp TranslateEnd
+
+DoDas:
+    jsr TryMove         ; Try to move by the delta already on the stack.
+    lda #DAS_REPEAT     ; Reset the DAS timer to the "repeat" interval.
     sta DasTimer
 
-
-End:
+TranslateEnd:
     lda JOY1L                       ; Read controller status
     sta PrevInput                   ; Store it in PrevInput
 
+    pla                             ; Clean up the stack.
     pla
-    pla
-
-    ; lda PrevInput                   ; Load last frame's status.
-    ; eor #$ffff
-    ; and $01, S                      ; A = current status - last frame's status (i.e., new buttons)
-
-    ; bit #RIGHT_BUTTON               ; If right is pressed, move right, etc.
-    ; bne MoveRight
-
-    ; lda #INIT_DAS_DELAY
-
-
-; MoveRight:
-
-
-        ; bit #LEFT_BUTTON
-        ; bne MoveLeft
-
-        ; bit #DOWN_BUTTON
-        ; bne MoveDown
-
-        ; bit #UP_BUTTON
-        ; bne MoveUp
-
-        ; jmp MoveEnd
-
-; MoveLeft:
-;         pea -1
-;         jsr TryMove
-;         pla
-;         jmp MoveEnd
-
-; MoveRight:
-;         pea $0001
-;         jsr TryMove
-;         pla
-;         jmp MoveEnd
-
-; MoveDown:
-;         pea $20
-;         jsr TryMove
-;         pla
-;         jmp MoveEnd
-
-; MoveUp:
-;         jsr TryRotate
-;         jmp MoveEnd
-
-; MoveEnd:
-
-    ; pla                             ; Pop controller input
-    ; sta PrevInput                   ; Store it in PrevInput
     rts
 
 .endproc
@@ -166,8 +121,28 @@ WaitForJoypad:
 
     ; First read the rotation status.
 
-    ; TODO
+    lda PrevInput                   ; Load last frame's status.
+    eor #$FFFF                      ; bitwise NOT
+    and JOY1L                       ; A = current status - last frame's status (i.e., new buttons)
 
+    bit #B_BUTTON
+    bne Rotate
+    bit #A_BUTTON
+    bne Counter
+    jmp RotationEnd
+
+Rotate:
+    lda InputStatus, S
+    ora #INPUT_STATUS_ROTATE
+    sta InputStatus, S
+    jmp RotationEnd
+
+Counter:
+    lda InputStatus, S
+    ora #INPUT_STATUS_COUNTER
+    sta InputStatus, S
+
+RotationEnd:
     ; Next, see which direction we're moving.
 
     lda JOY1L                       ; Read controller status
